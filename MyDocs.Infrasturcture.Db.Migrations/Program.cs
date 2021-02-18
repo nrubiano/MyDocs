f69e0508d1a5e0045ff7303ef61b6ab2e3a51750
Nicolas Rubiano <nicolas.rubiano@outlook.com>
2021-02-17 22:31:59 -0500

    add docker orquestration

---
+++ MyDocs.Infrasturcture.Db.Migrations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDocs.Infrastructure.Db.Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
