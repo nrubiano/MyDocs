6fa731cb1eef162beecb477100ac9b339b304c4c
Nicolas Rubiano <nicolas.rubiano@outlook.com>
2021-02-21 23:25:11 -0500

    add the skeleton structure

---
+++ MyDocs.Hosts.Backend/Controllers/HealthController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyDocs.Application.Health.Commands;
+
+namespace MyDocs.Hosts.Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : BaseController
+    {
+        
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Unit>> Get()
+        {
+            return await Mediator.Send(new HealthCheckCommand());
+        }
+    }
+}
