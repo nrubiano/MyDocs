bfe18e3710123a2ad95d67c264050e4603dc1428
Nicolas Rubiano <nicolas.rubiano@outlook.com>
2021-02-27 15:57:26 -0500

    add validator for the CreateGroupCommand

--- MyDocs.Application.IntegrationTests/GroupCommandsTests.cs
+++ MyDocs.Application.IntegrationTests/GroupCommandsTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using FluentAssertions;
+﻿using FluentAssertions;
 using FluentValidation;
 using MyDocs.Application.Groups.Commands.CreateGroup;
 using Xunit;
---
+++ MyDocs.Application/Groups/Commands/CreateGroup/CreateGroupCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace MyDocs.Application.Groups.Commands.CreateGroup
+{
+    public class CreateGroupCommandValidator : AbstractValidator<CreateGroupCommand>
+    {
+        public CreateGroupCommandValidator()
+        {
+            RuleFor(v => v.Name)
+                .NotEmpty();
+        }
+    }
+}
