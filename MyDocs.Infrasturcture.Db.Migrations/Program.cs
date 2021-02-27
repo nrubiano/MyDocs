62d6f84058d1ce13144a9a28767e4e80dd387e1c
Nicolas Rubiano <nicolas.rubiano@outlook.com>
2021-02-27 16:52:54 -0500

    add retry to the migrations

--- MyDocs.Infrasturcture.Db.Migrations/Program.cs
+++ MyDocs.Infrasturcture.Db.Migrations/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Reflection;
 using DbUp;
+using Npgsql;
 
 namespace MyDocs.Infrastructure.Db.Migrations
 {
@@ -13,6 +14,8 @@ namespace MyDocs.Infrastructure.Db.Migrations
                 args.FirstOrDefault()
                 ?? "Server=mydocs.database;Port=5432;Database=mydocs;User Id=mydocs;Password=h2E@93%5b!fVX*e=;";
 
+            WaitUntilDbIsReady(connectionString);
+            
             var upgrader =
                 DeployChanges.To
                     .PostgresqlDatabase(connectionString)
@@ -35,6 +38,27 @@ namespace MyDocs.Infrastructure.Db.Migrations
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Success!");
             Console.ResetColor();
+        }
+
+        private static void WaitUntilDbIsReady(string connectionString)
+        {
+            var isDbStarting = true;
+            while (isDbStarting)
+            {
+                try
+                {
+                    using var conn = new NpgsqlConnection(connectionString);
+                    conn.Open();
+
+                    Console.WriteLine("Db ready apply migrations");
+
+                    isDbStarting = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Db is not ready yet, waiting... :S");
+                }
+            }
         }
     }
 }
